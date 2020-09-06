e9cc0bb686ed5213381c9ccdb7530f121ffc6211
jenthura <joshuasth@gmail.com>
2020-09-06 02:12:17 -0500

    changed from localhost again again

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `https://rocky-garden-73910.herokuapp.com/`,
+  API_ENDPOINT: `https://rocky-garden-73910.herokuapp.com`,
   API_KEY: process.env.REACT_APP_API_KEY,
 };
